514464daf247f49799f5a49ab2b6118eb3740b52
ivan <ierodriguezgo@gmail.com>
2020-12-02 13:22:38 -0500

    resolvers and schema added

---
+++ db/resolvers.js
@@ -0,0 +1,29 @@
+const { gql } = require('apollo-server');
+
+const cursos = [
+  {
+      titulo: 'JavaScript Moderno Guía Definitiva Construye +10 Proyectos',
+      tecnologia: 'JavaScript ES6',
+  },
+  {
+      titulo: 'React – La Guía Completa: Hooks Context Redux MERN +15 Apps',
+      tecnologia: 'React',
+  },
+  {
+      titulo: 'Node.js – Bootcamp Desarrollo Web inc. MVC y REST API’s',
+      tecnologia: 'Node.js'
+  }, 
+  {
+      titulo: 'ReactJS Avanzado – FullStack React GraphQL y Apollo',
+      tecnologia: 'React'
+  }
+];
+
+const resolvers = {
+  Query: {
+    obtenerCursos: () => cursos,
+    obtenerTecnologia: () => cursos[0]
+  }
+}
+
+module.exports = resolvers;
--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-const { ApolloServer, gql } = require('apollo-server')
+const { ApolloServer } = require('apollo-server');
+const typeDefs = require ('./db/schema');
+const resolvers = require('./db/resolvers');
 
-const server = new ApolloServer();
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+});
 
 server.listen().then(({ url }) => {
   console.log(`Servidor escuchando en ${url}`)
